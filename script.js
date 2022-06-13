6efafc4fb0104f6bbf831ef2adceb76b75b70d48
TIAGO DA SILVA SOUZA <72765859+titicos@users.noreply.github.com>
2022-06-12 22:57:32 -0400

    firstcommit

---
+++ script.js
@@ -0,0 +1,16 @@
+function escurecerTela(){
+ 
+    var corpo = document.querySelector("body")
+    corpo.style.backgroundColor="rgb(105, 100, 100)"  
+
+}
+
+function clarearTela(){
+ 
+    var corpo = document.querySelector("body")
+    corpo.style.backgroundColor="white"  
+
+    var botao =document.querySelector("btnClaro")
+    botao.style.backgroundColor="black"
+
+}
